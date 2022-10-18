9f7da8388aa1b56d88909a8388f4f0bcfb6733c0
Pedro Henrique Appendino <eng.appendino@gmail.com>
2022-10-18 16:26:04 -0300

    change module version

--- terraform/account/us-east-1/iam/main.tf
+++ terraform/account/us-east-1/iam/main.tf
@@ -2,6 +2,6 @@ variable "name" {
     type = string
 }
 module "iam_account" {
-   source = "terraform-aws-modules/iam/aws//modules/iam-user"
+   source = "terraform-aws-modules/iam/aws//modules/iam-user?version=5.4.0"
    name = var.name
 }
